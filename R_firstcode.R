0851464599bd3aeebec8b5e695daa9418c0a1654
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-03-10 17:18:41 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -3,4 +3,14 @@
 
 install.packages("raster")
 
-library("raster")
+library(raster)
+
+#Import data setting the working directory
+setwd("C:/lab/") #windows
+
+# import
+l2011 <- brick("p224r63_2011_masked.grd")
+l2011
+
+# plot
+plot(l2011)
