70d4e99ccaee1b38cda9e7bf358e0bbf63e368c7
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-03-23 17:38:27 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -59,3 +59,36 @@ plot(l2011[[3]], col=clr)
 clinf <- colorRampPalette(c("darkorchid4", "darkorchid1", "darkorchid")) (100)
 plot(l2011[[4]], col=clinf)
 
+#Exercise: plot the NIR band
+plot(l2011[[4]])
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+#Exersice: import the 1988 image
+setwd("C:/lab/")
+l1988 <- brick("p224r63_1988_masked.grd")
+l1988
+#Exersice: plot in RGB space (natural colours)
+plotRGB(l1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")#falso colori
+plotRGB(l1988, 4, 3, 2, stretch="Lin")#uguale come quello sopra
+
+#multiframe
+par(mfrow=c(2,1))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+dev.off()
+
+#histogrammi
+plotRGB(l1988, 4, 3, 2, stretch="Hist")
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
+#exersice multiframe with 4 images
+par(mfrow=c(2,2))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+plotRGB(l1988, 4, 3, 2, stretch="Hist")
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
