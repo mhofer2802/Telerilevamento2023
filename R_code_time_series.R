ef5db56227ebf2841ef41210b32966a09e14d398
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-04-13 17:06:30 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -30,7 +30,38 @@ dift<-TGR[[2]]-TGR[[1]]
 plot(dift)
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 plot(TGr, col=cl)
-# levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
-#
-# levelplot(TGr,col.regions=cl, main="LST variation in time",
-#           names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+#-----Exercise 2: European NO2
+setwd("C:/lab/EN") 
+
+#importing a file
+en_first <- raster("EN_0001.png")
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) #
+plot(en_first, col=cl)
+
+rlist<- list.files(pattern="EN")
+rimp <- lapply(rlist, raster)
+plot(en, col=cl)
+
+#check
+par(mfrow=c(1,2))
+plot(en_first, col=cl)
+plot(en[[1]], col=cl)
+#check 2
+difcheck<- en_first-en[[1]]
+difcheck
+plot(difcheck)
+
+#first and last data
+par(mfrow=c(1,2))
+plot(en_first, col=cl)
+plot(en[[13]], col=cl)
+
+difen=en[[1]]-en[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100) #
+plot(difen, col=cldif)
+
+# plotRGB of three files together
+plotRGB(en, r=1, g=7, b=13, stretch="lin")
+plotRGB(en, r=1, g=7, b=13, stretch="hist")
