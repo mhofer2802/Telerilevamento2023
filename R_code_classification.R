07418c3ab92606b45dc5430ea7a81bf41c3e2134
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-04-20 17:38:46 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -1,10 +1,10 @@
 #classification of remote sensing data via RStoolbox
 
-#Installing devools (fuori dal CRAN)
+#Installing devools 
 install.packages("devtools")
 library(devtools)
 #devtools::install_github("bleutner/RStoolbox")
-library(RStoolbox)
+library(RStoolbox)#(fuori dal CRAN--> problem?)
 
 
 
@@ -41,3 +41,32 @@ percentages
 #class 2(nero): highest energy level: 21.21%
 #class 1(giallo): lowest energy level:37.33%
 #class 3 (rosso): medium energy level:41.45%
+
+#----Gand Canyon exercise
+gc<-brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+#the image is quite big
+gcc<- crop(gc, drawExtent())
+gcc
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+ncell(gcc)#nummero di pixel
+
+#1. get values
+singlenr <- getValues(gcc)
+singlenr
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
+# 3. Set values
+gcclass <- setValues(gcc[[1]], kcluster$cluster) # assign new values to a raster object
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(gcclass, col=cl)
+#classe1=giallo:sandstone
+#classe2=nero:coglomerates
+#classe3=red:volcanic rocks
+
+frequencies <- freq(gcclass)
+tot = 58076148
+total<-ncell(gcclass)
+percentages = frequencies * 100 /  tot
