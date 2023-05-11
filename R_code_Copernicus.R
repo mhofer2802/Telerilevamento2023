8a6fecfcc6a974be700ffa9aef54a0a4625e359b
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-05-11 17:48:23 +0200

    Create R_code_Copernicus.R

---
+++ R_code_Copernicus.R
@@ -0,0 +1,12 @@
+#R code for downloading and visualizing Copernicus data
+
+#install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+setwd("C:/lab/") #Windows
+
+#register and downloaad
+#https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+#dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
