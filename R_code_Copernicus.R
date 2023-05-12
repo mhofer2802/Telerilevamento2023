559522a3ff68146b677fdd45d9a4cd17dd535b21
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-05-12 17:32:15 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -18,3 +18,25 @@ plot(ssoil)
 
 scd<-as.data.frame(ssoil,xy=T)
 head(scd)
+
+ggplot() + 
+  geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+  ggtitle("Soil Moisture from Copernicus")
+
+
+#Cropping an image
+ext<- c(23,30,62,68)
+sc.crop<-crop(ssoil, ext)#nome del datum e estenzione che e ext
+sc.crop
+plot(sc.crop)
+sc.cropdatafram<-as.data.frame(sc.crop,xy=T)
+head(sc.cropdatafram)
+ggplot() +
+  geom_raster(sc.cropdatafram, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))
+# with viridis
+ggplot() +
+  geom_raster(sc.cropdatafram, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+  scale_fill_viridis(option= "cividis")
+
+  
+  
