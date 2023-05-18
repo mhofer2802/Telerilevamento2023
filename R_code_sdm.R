6ea8a31b589bfc54fd7da5d741eec2ace1292d6d
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-05-18 16:32:42 +0200

    Create R_code_sdm.R

---
+++ R_code_sdm.R
@@ -0,0 +1,9 @@
+#R code for species distribution modelling
+# Species Distribution Modelling
+
+#install.packages("sdm")
+# install.packages("rgdal", dependencies=T)
+
+library(sdm)
+library(raster) # predictors
+library(rgdal) # species
