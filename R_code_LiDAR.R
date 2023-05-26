1f72a54cfbdf96229368e7a7ea67e791786693c5
Mirjam Hofer <127043525+mhofer2802@users.noreply.github.com>
2023-05-26 16:55:40 +0200

    Create R_code_LiDAR.R

---
+++ R_code_LiDAR.R
@@ -0,0 +1,9 @@
+#3D in R
+
+setwd("~/lab/dati/")
+library(raster) #"Geographic Data Analysis and Modeling"
+library(rgdal) #"Geospatial Data Abstraction Library"
+library(viridis)
+library(ggplot2)
+
+raster("2013Elevation_DigitalElevationModel-0.5m.tif"
